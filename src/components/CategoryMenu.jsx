75790cc73ebf176d45f7266017c2be7fd777ee46
Abdus Samad <abdussamadshamsi486@gmail.com>
2025-02-13 15:14:11 +0530

    Bugs Solved

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,42 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import FoodData from '../data/FoodData';
-import { useDispatch,useSelector } from 'react-redux';
-import { setCategory as setReduxCategory } from '../redux/CategorySlice';
+import React, { useEffect, useState } from "react";
+import FoodData from "../data/FoodData";
+import { useDispatch, useSelector } from "react-redux";
+import { setCategory } from "../redux/CategorySlice";
 
 const CategoryMenu = () => {
-  const [localCategory, setLocalCategory] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const listUniqueCategories = () => {
+    const uniqueCategories = [
+      ...new Set(FoodData.map((food) => food.category)),
+    ];
+    setCategories(uniqueCategories);
+    console.log(uniqueCategories);
+  };
 
   useEffect(() => {
-    const uniqueCategories = [...new Set(FoodData.map((food) => food.category))];
-    setLocalCategory(uniqueCategories);
+    listUniqueCategories();
   }, []);
 
   const dispatch = useDispatch();
-  const selectedCategory = useSelector((state) => state.category.category)
+  const selectedCategory = useSelector((state) => state.category.category);
 
   return (
     <div className="ml-6">
-      <h3 className="text-xl font-semibold mt-8">Find The Best Food</h3>
-
+      <h3 className="text-xl font-semibold">Find the best food</h3>
       <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden">
-      <button
-          onClick={() => dispatch(setReduxCategory("All"))}
+        <button
+          onClick={() => dispatch(setCategory("All"))}
           className={`px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white ${
             selectedCategory === "All" && "bg-green-500 text-white"
           }`}
         >
           All
         </button>
-        {localCategory.map((cat, index) => (
-          <button
-            onClick={() => {dispatch(setReduxCategory(cat));
-            }}
-            key={index}
-            className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white"
-          >
-            {cat}
-          </button>
-        ))}
+        {categories.map((category, index) => {
+          return (
+            <button
+              onClick={() => dispatch(setCategory(category))}
+              key={index}
+              className={`px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white ${
+                selectedCategory === category && "bg-green-500 text-white"
+              } `}
+            >
+              {category}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
--- src/components/Fooditems.jsx
+++ src/components/Fooditems.jsx
@@ -1,34 +1,40 @@
 import React from "react";
-import Card from "./Card";
-import FoodData from "../data/FoodData";
-import toast, {Toaster} from "react-hot-toast";
+import Card from './Card.jsx'
+import FoodData from "../data/FoodData.js";
+import toast, { Toaster } from "react-hot-toast";
 import { useSelector } from "react-redux";
 
 const Fooditems = () => {
   const category = useSelector((state) => state.category.category);
-  const handleToast = (name) => toast.success(`Added ${name}`);
-
+  const search = useSelector((state) => state.search.search);
+  const handleToast = (name) => toast.success(`Added ${name} `);
   return (
     <>
-    <Toaster position="top-center" reverseOrder={false} />
-    
-    <div className="flex flex-wrap gap-10 justify-center lg:justify-start mx-6 my-10">
-      {FoodData.filter((food) => category === "All" || food.category === category).map((food) => (
-        <Card
-          key={food.id}
-          id={food.id}
-          img={food.img}
-          name={food.name}
-          price={food.price}
-          desc={food.desc}
-          category={food.category}
-          rating={food.rating}
-          handleToast={handleToast}
-        />
-      ))}
-    </div>
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="flex flex-wrap gap-10 justify-center lg:justify-start mx-6 my-10">
+        {FoodData.filter((food) => {
+          if (category === "All") {
+            return food.name.toLowerCase().includes(search.toLowerCase());
+          } else {
+            return (
+              category === food.category &&
+              food.name.toLowerCase().includes(search.toLowerCase())
+            );
+          }
+        }).map((food) => (
+          <Card
+            key={food.id}
+            id={food.id}
+            name={food.name}
+            price={food.price}
+            desc={food.desc}
+            rating={food.rating}
+            img={food.img}
+            handleToast={handleToast}
+          />
+        ))}
+      </div>
     </>
-    
   );
 };
 
